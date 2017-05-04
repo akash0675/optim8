caee746c22da067a783a975affb8b3337d74c9ba
akashtalreja <akash@levelz.in>
2017-05-05 00:59:24 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/optim8.scss',
+  output: {
+    filename: 'dist/optim8.bundle.css',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader?importLoaders=1',
+        }),
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'dist/optim8.bundle.css',
+      allChunks: true,
+    }),
+  ],
+};
